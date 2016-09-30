fd10edd6849a0b75adeb4da5818722eac1d4e606
jgmontoya <jgmontoya@290JGMONTOYA_P.Grupo-Exito.com>
2016-09-30 15:53:02 -0500

    Ejemplo clase

---
+++ src/Humano.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jgmontoya
+ */
+public class Humano extends SerVivo{
+    
+    @Override
+    public void comunicarse(){   
+        System.out.println("BLA BLA BLA ....");
+    }
+    
+    
+}
---
+++ src/Pato.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jgmontoya
+ */
+public class Pato extends SerVivo{
+    
+    @Override
+    public void comunicarse(){   
+        System.out.println("QUAK QUAK QUAK ....");
+    }
+}
